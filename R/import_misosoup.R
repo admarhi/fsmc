1f0ee0acc6e051319371d65e533a5f1aade2185c
Adrian Hirt <am.hirt@pm.me>
2024-04-30 15:36:27 +0200

    added misosoup import function

--- R/import_misosoup.R
+++ R/import_misosoup.R
@@ -11,31 +11,37 @@ sol_to_exchange_network = function(sol_tmp,met_medium,filterLow=1e-03){
   # date: April 25th, 2022 (Berlin)
 
   #browser()
-  sol_tmp=unlist(sol_tmp) # convert solution in a named vector
-  idx_ex=grep("_e_",names(sol_tmp)) # identify exchanged reactions
-  if(length(idx_ex)==0){
+  sol_tmp = unlist(sol_tmp) # convert solution in a named vector
+  idx_ex = grep("_e_", names(sol_tmp)) # identify exchanged reactions
+  if (length(idx_ex) == 0) {
     return()
   }
-  sol_tmp=sol_tmp[idx_ex] # select only those
-  idx_ex=c() # among reactions
-  for(met in met_medium){ # exclude those involving metab present in the base medium
-    idx_ex_tmp=grep(met,names(sol_tmp))
-    idx_ex=c(idx_ex,idx_ex_tmp)
+  sol_tmp = sol_tmp[idx_ex] # select only those
+  idx_ex = c() # among reactions
+  for (met in met_medium) {
+    # exclude those involving metab present in the base medium
+    idx_ex_tmp = grep(met, names(sol_tmp))
+    idx_ex = c(idx_ex, idx_ex_tmp)
   }
-  matched=match(seq(1,length(sol_tmp)),idx_ex) # take a list of indexes of sol_tmp and check which are not in idx_ex
-  sol_tmp=sol_tmp[is.na(matched)] # we are looking for those not found
+  matched = match(seq(1, length(sol_tmp)), idx_ex) # take a list of indexes of sol_tmp and check which are not in idx_ex
+  sol_tmp = sol_tmp[is.na(matched)] # we are looking for those not found
   # ... finally, exclude reactions that are too small
-  idx_high=which(abs(sol_tmp) > filterLow)
-  sol_tmp=sol_tmp[idx_high]
+  idx_high = which(abs(sol_tmp) > filterLow)
+  sol_tmp = sol_tmp[idx_high]
   #names(sol_tmp) # double check
-  sol.names=names(sol_tmp)
-  net.names=t(sapply(sol.names,FUN=function(x){stri_split(x,fixed="_e_")[[1]]}))
-  net.names=as.data.frame(net.names)
-  colnames(net.names)=c("nodeA","nodeB")
-  net.names$nodeA=stri_replace_first(net.names$nodeA,fixed="R_EX_",replacement="")
-  net.names$nodeB=stri_replace_first(net.names$nodeB,fixed="_i",replacement="")
-  net.df=net.names
-  net.df$weight=sol_tmp
-  rownames(net.df)=seq(1,dim(net.df)[1],1)
+  sol.names = names(sol_tmp)
+  net.names = t(sapply(
+    sol.names,
+    FUN = function(x) { stringi::stri_split(x, fixed = "_e_")[[1]] }
+  ))
+  net.names = as.data.frame(net.names)
+  colnames(net.names) = c("nodeA", "nodeB")
+  net.names$nodeA <- stringi::stri_replace_first(
+    net.names$nodeA, fixed = "R_EX_", replacement = "")
+  net.names$nodeB = stringi::stri_replace_first(
+    net.names$nodeB, fixed = "_i", replacement = "")
+  net.df = net.names
+  net.df$weight = sol_tmp
+  rownames(net.df) = seq(1, dim(net.df)[1], 1)
   return(net.df)
 }
