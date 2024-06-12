84bef529150ab995cad56fc80810ff9a553ecb73
Adrian Hirt <am.hirt@pm.me>
2024-06-12 17:43:10 +0200

    additional ex data

--- data-raw/example-communities.R
+++ data-raw/example-communities.R
@@ -1,3 +1,6 @@
 #### MisoSoup ####
 ac_A1R12_1 <- getCo(MiCo("inst/extdata/misosoupR/ac_A1R12_1.csv"))
 usethis::use_data(ac_A1R12_1, overwrite = TRUE)
+
+cit_A1R12_1 <- getCo(MiCo("inst/extdata/misosoupR/cit_A1R12_1.csv"))
+usethis::use_data(cit_A1R12_1, overwrite = TRUE)
