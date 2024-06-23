3c41dcca90c22dc1660ad4f1dc57b673e7dc57d2
Adrian Hirt <am.hirt@pm.me>
2024-06-23 22:23:16 +0200

    housekeeping

--- data-raw/example-communities.R
+++ data-raw/example-communities.R
@@ -1,6 +1,10 @@
 #### MisoSoup ####
-ac_A1R12_1 <- getCo(MiCo("inst/extdata/misosoup/ac_A1R12_1.csv"))
+ac_A1R12_1 <-
+  read.csv("inst/extdata/misosoup/ac_A1R12_1.csv") %>%
+  tibble::as_tibble()
 usethis::use_data(ac_A1R12_1, overwrite = TRUE)
 
-cit_A1R12_1 <- getCo(MiCo("inst/extdata/misosoup/cit_A1R12_1.csv"))
+cit_A1R12_1 <-
+  read.csv("inst/extdata/misosoup/cit_A1R12_1.csv") %>%
+  tibble::as_tibble()
 usethis::use_data(cit_A1R12_1, overwrite = TRUE)
