f71c31487e269e25e366506c601b730b073dc1ba
Adrian Hirt <am.hirt@pm.me>
2024-06-23 21:06:34 +0200

    updated example data

--- data-raw/example-communities.R
+++ data-raw/example-communities.R
@@ -1,6 +1,6 @@
 #### MisoSoup ####
-ac_A1R12_1 <- getCo(MiCo("inst/extdata/misosoupR/ac_A1R12_1.csv"))
+ac_A1R12_1 <- getCo(MiCo("inst/extdata/misosoup/ac_A1R12_1.csv"))
 usethis::use_data(ac_A1R12_1, overwrite = TRUE)
 
-cit_A1R12_1 <- getCo(MiCo("inst/extdata/misosoupR/cit_A1R12_1.csv"))
+cit_A1R12_1 <- getCo(MiCo("inst/extdata/misosoup/cit_A1R12_1.csv"))
 usethis::use_data(cit_A1R12_1, overwrite = TRUE)
