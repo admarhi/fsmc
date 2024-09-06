c1e0442019d54b4391f1ec1c31d1eb1855662793
Adrian Hirt <am.hirt@pm.me>
2024-09-06 13:25:42 +0200

    test set 2

--- tests/testthat/fixtures/alignment_test_files.R
+++ tests/testthat/fixtures/alignment_test_files.R
@@ -1,16 +1,57 @@
-tb_set1 <- 
-  tibble::tribble(
+tibble::tribble(
+  ~uptake, ~secretion, ~flux, ~species,
+  "m1", "m2", 1, "s1",
+  "m2", "m3", 1, "s2",
+  "m3", "m4", 1, "s3",
+  "m4", "m1", 1, "s4"
+) %>%
+  pivotMiCo(
+    species = "species",
+    from = "uptake",
+    to = "secretion",
+    flux = "flux"
+  ) %>%
+  readr::write_rds("tests/testthat/fixtures/aligment_test_set1.rds")
+
+# Set2
+list(
+  c1 = tibble::tribble(
     ~uptake, ~secretion, ~flux, ~species,
     "m1", "m2", 1, "s1",
     "m2", "m3", 1, "s2",
     "m3", "m4", 1, "s3",
-    "m4", "m1", 1, "s4"
-  ) %>% pivotMiCo( 
-  species = "species", 
-  from = "uptake", 
-  to = "secretion", 
-  flux = "flux")
-readr::write_rds(tb_set1, "tests/testthat/fixtures/alig_test_set1.rds")
-
-
-
+    "m4", "m1", 1, "s4",
+    "m1", "m4", 1, "s5",
+  ),
+  c2 = tibble::tribble(
+    ~uptake, ~secretion, ~flux, ~species,
+    "m1", "m2", 1, "s1",
+    "m2", "m3", 1, "s2",
+    "m3", "m4", 1, "s3",
+    "m4", "m1", 1, "s4",
+    "m4", "m3", 1, "s5",
+  ),
+  c3 = tibble::tribble(
+    ~uptake, ~secretion, ~flux, ~species,
+    "m1", "m2", 1, "s1",
+    "m2", "m3", 1, "s2",
+    "m3", "m4", 1, "s3",
+    "m4", "m1", 1, "s4",
+    "m3", "m2", 1, "s5",
+  ),
+  c4 = tibble::tribble(
+    ~uptake, ~secretion, ~flux, ~species,
+    "m1", "m2", 1, "s1",
+    "m2", "m3", 1, "s2",
+    "m3", "m4", 1, "s3",
+    "m4", "m1", 1, "s4",
+    "m2", "m1", 1, "s5",
+  )
+) %>%
+  purrr::map(pivotMiCo,
+    species = "species",
+    from = "uptake",
+    to = "secretion",
+    flux = "flux"
+  ) %>%
+  readr::write_rds("tests/testthat/fixtures/aligment_test_set2.rds")
--- tests/testthat/test-scoreAlignment.R
+++ tests/testthat/test-scoreAlignment.R
@@ -1,10 +1,18 @@
 test_that("alignment scoring produces expected results", {
   
-  # Read in the test alignment test files 
-  tb_set1 <- readr::read_rds(test_path("fixtures", "alig_test_set1.rds"))
-  
-  a1 <- newMiCoAl(newMiCo(tb_set1, name = "t1"), newMiCo(tb_set1, name = "t2"))
+  # Set 1
+  tb1 <- readr::read_rds(test_path("fixtures", "aligment_test_set1.rds"))
+  a1 <- newMiCoAl(newMiCo(tb1, name = "t1"), newMiCo(tb1, name = "t2"))
   s1 <- .scoreAlignment(a1@alignment)
   expect_equal(nrow(s1), 1)
   expect_equal(s1$score, 1)
+
+  # Set 2
+  a2 <- 
+    readr::read_rds(test_path("fixtures", "aligment_test_set2.rds")) %>% 
+    purrr::imap(newMiCo) %>% 
+    newMiCoAl()
+  s2 <- .scoreAlignment(a2@alignment)
+  expect_equal(nrow(s2), 1)
+  expect_equal(s2$score, 0.5)
 })
