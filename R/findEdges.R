ca5dfed5c56a39ee6cabc1ce141af835f8049484
Adrian Hirt <am.hirt@pm.me>
2024-09-12 12:21:14 +0200

    legacy toggle

--- R/findEdges.R
+++ R/findEdges.R
@@ -5,31 +5,29 @@
 #' between species.
 #'
 #' @param tb Tibble describing a microbial community
+#' @param legacy Boolean to toggle legacy behavior
 #'
-#' @return A list of two hashs, one for edges between metabolites and one for
+#' @return A hash containing the edges of the community. The legacy output
+#' returns a list of two hashs, one for edges between metabolites and one for
 #' species.
 #' @export
 #'
 #' @examples
 #' findEdges(ac_A1R12_1)
-findEdges <- function(tb) {
-
+findEdges <- function(tb, legacy = FALSE) {
   ### ToDo ---------------------------------------------------------------------
   ### - Replace with option to either get the species or the metabolite edges
   ### - Check that the columns are in the right order and correct names
   ### - Checks that the columns are of the expected types
   ### - Turn the above two into a general `checkValidMiCoTibble()` function
   ### - Ensure the downstream effects of changing the output are addressed
-  ### - Behaviour for only directional fluxes
+  ### - Decide on behaviour for only directional fluxes
   ### --------------------------------------------------------------------------
-
-  names(tb) <- c("species", "met", "flux")
 
   species <- unique(tb$species)
   metabolites <- unique(tb$met)
 
   met_hash <- hash::hash()
-  species_hash <- hash::hash()
 
   for (m in metabolites) {
     producers <- tb$species[tb$met == m & tb$flux > 0]
@@ -43,27 +41,31 @@ findEdges <- function(tb) {
       producers = producers,
       consumers = consumers,
       prod_fluxes = prod_fluxes,
-      cons_fluxes = cons_fluxes)
+      cons_fluxes = cons_fluxes
+    )
   }
+  if (legacy) {
+    species_hash <- hash::hash()
+    for (s in species) {
+      metabolites_in <- unique(tb$met[tb$species == s & tb$flux < 0])
+      metabolites_out <- unique(tb$met[tb$species == s & tb$flux > 0])
 
-  ### Add the flux to the output
-  for (s in species) {
-    metabolites_in <- unique(tb$met[tb$species == s & tb$flux < 0])
-    metabolites_out <- unique(tb$met[tb$species == s & tb$flux > 0])
+      species_from <- vector()
+      species_to <- vector()
 
-    species_from <- vector()
-    species_to <- vector()
+      for (m in metabolites_in) {
+        species_from <- c(species_from, met_hash[[m]]$producers)
+      }
 
-    for (m in metabolites_in) {
-      species_from <- c(species_from, met_hash[[m]]$producers)
+      for (m in metabolites_out) {
+        species_to <- c(species_to, met_hash[[m]]$consumers)
+      }
+
+      species_hash[[s]] <- hash::hash(from = species_from, to = species_to)
     }
 
-    for (m in metabolites_out) {
-      species_to <- c(species_to, met_hash[[m]]$consumers)
-    }
-
-    species_hash[[s]] <- hash::hash(from = species_from, to = species_to)
+    return(list(metabolites = met_hash, species = species_hash))
   }
 
-  return(list(metabolites = met_hash, species = species_hash))
+  return(met_hash)
 }
