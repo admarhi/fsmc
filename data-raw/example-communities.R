f9378d9af962b344fd62e7a5c1a0ef6c3a92874d
Adrian Hirt <am.hirt@pm.me>
2025-02-24 22:18:23 -0500

    consolidated test data

--- data-raw/example-communities.R
+++ data-raw/example-communities.R
@@ -1,35 +1,9 @@
-#### MisoSoup ####
-ac_A1R12_1 <-
-  read.csv("inst/extdata/misosoup/ac_A1R12_1.csv") %>%
-  tibble::as_tibble()
-usethis::use_data(ac_A1R12_1, overwrite = TRUE)
-
-ac_A1R12_2 <-
-  read.csv("inst/extdata/misosoup/ac_A1R12_2.csv") %>%
-  tibble::as_tibble()
-usethis::use_data(ac_A1R12_2, overwrite = TRUE)
-
-cit_A1R12_1 <-
-  read.csv("inst/extdata/misosoup/cit_A1R12_1.csv") %>%
-  tibble::as_tibble()
-usethis::use_data(cit_A1R12_1, overwrite = TRUE)
-
-cit_A1R12_2 <-
-  read.csv("inst/extdata/misosoup/cit_A1R12_2.csv") %>%
-  tibble::as_tibble()
-usethis::use_data(cit_A1R12_2, overwrite = TRUE)
-
-ac_A3R04_1 <-
-  read.csv("inst/extdata/misosoup/ac_A3R04_1.csv") %>%
-  tibble::as_tibble()
-usethis::use_data(ac_A3R04_1, overwrite = TRUE)
-
-misosoup <- list()
+misosoup24 <- list()
 for (f in fs::dir_ls("inst/extdata/misosoup")) {
   name <- f %>%
     fs::path_file() %>%
     fs::path_ext_remove()
   tb <- read.csv(f) %>% tibble::as_tibble()
-  misosoup[[name]] <- tb
+  misosoup24[[name]] <- tb
 }
-usethis::use_data(misosoup, overwrite = TRUE)
+usethis::use_data(misosoup24, overwrite = TRUE)
